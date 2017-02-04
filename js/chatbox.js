2f554b51a69271155e78915b181cea66a23549dd
Cobrand <cobrandw@gmail.com>
2017-02-04 23:16:16 +0100

    Core chatbix

--- js/chatbox.js
+++ js/chatbox.js
@@ -1,1 +1,83 @@
-console.log("Chatbox has started");
+console.log("Chatbix has started");
+var vue = new Vue({
+    el: '#app',
+    data: {
+        focus: true,
+        heartbeat_lock: false,
+        messages:[],
+        last_timestamp:null,
+        username:localStorage.username || '',
+        unread_message_count:0,
+        color:localStorage.usercolor || '#222222',
+        preferences:{
+            notify_on_mention:true,
+            auto_scroll:true,
+        }
+    },
+    methods: {
+        load_messages:function(messages,override){
+            if (messages.length !== 0) {
+                if (this.last_timestamp == null) {
+                    this.last_timestamp = messages[messages.length - 1].timestamp;
+                } else {
+                    this.last_timestamp = Math.max(messages[messages.length - 1].timestamp, override ? 0 : this.last_timestamp);
+                }
+            }
+            if (this.messages.length === 0 || override) {
+                this.messages = messages;
+            } else {
+                messages.forEach(function(m){
+                    this.messages.push(m);
+                }.bind(this))
+            };
+        },
+        heartbeat:function(){
+            if (!this.heartbeat_lock) {
+                this.heartbeat_lock = true;
+                var modifiers = {};
+                if (this.username != "") {
+                    modifiers["username"] = this.username;
+                }
+                if (this.last_timestamp != null){
+                    modifiers["timestamp"] = this.last_timestamp;
+                }
+                var heartbeat_url = "/api/heartbeat" + AJAX.urlparams_to_string(modifiers);
+                AJAX.get(heartbeat_url,function(req){
+                    this.heartbeat_lock = false;
+                    this.load_messages(req.responseJSON.messages);
+                }.bind(this),
+                function(e,req) {
+                    this.heartbeat_lock = false;
+                    console.log("Error in heartbeat:");
+                    console.log(e);
+                }.bind(this)
+                );
+            }
+        },
+        loop_heartbeat:function(){
+            this.heartbeat();
+            setTimeout(this.loop_heartbeat.bind(this),this.focus ? 750 : 5000)
+        },
+        startup:function(){
+            this.loop_heartbeat();
+            window.onblur = this.on_blur.bind(this);
+            window.onfocus = this.on_focus.bind(this);
+        },
+        on_focus:function(){
+            this.active = true
+        },
+        on_blur:function(){
+            this.active = false;
+        }
+    },
+    watch:{
+        username: function(new_username) {
+            localStorage.username = new_username;
+        },
+        color: function(new_color) {
+            localStorage.usercolor = new_color;
+        }
+    }
+});
+
+vue.startup();
---
+++ js/utils.js
@@ -0,0 +1,45 @@
+var AJAX = {
+  get : function(url,response_fun,error_fun) {
+    var request = new XMLHttpRequest();
+    request.open('GET',url, true);
+    request.onload = function() {
+      request.responseJSON = JSON.parse(request.responseText) ;
+      response_fun(request);
+    };
+    request.onerror = function(e) {
+      request.responseJSON = JSON.parse(request.responseText) ;
+      if (error_fun) {
+        error_fun(e,request);
+      }
+    };
+    request.send();
+  },
+  // parameters must be an object
+  post : function(url,parameters,response_fun,error_fun) {
+    var request = new XMLHttpRequest();
+    request.open('POST',url, true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.onload = function() {
+      request.responseJSON = JSON.parse(request.responseText) ;
+      response_fun(request);
+    };
+    request.onerror = function(e) {
+      request.responseJSON = JSON.parse(request.responseText) ;
+      if (error_fun) {
+        error_fun(e,request);
+      }
+    };
+    request.send(JSON.stringify(parameters));
+  },
+  urlparams_to_string:function(urlparams) {
+    var string = "";
+    for (var param in urlparams) {
+      if (string == "") {
+        string += "?"+param+"="+urlparams[param];
+      } else {
+        string += "&"+param+"="+urlparams[param];
+      }
+    }
+    return string;
+  },
+}
